00bb3bce68e2691442ba8447afd66b888251cd2b
Simon Knittel <hallo@simonknittel.de>
2021-03-05 20:18:53 +0100

    Add react-markdown to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,10 @@
 import * as React from "react"
+import ReactMarkdown from "react-markdown"
 
 import "./Footer.scss"
 
 export default function Footer({ content }) {
-  return <footer className="Footer" dangerouslySetInnerHTML={{__html: content}} />
+  return <footer className="Footer">
+    <ReactMarkdown>{ content }</ReactMarkdown>
+  </footer>
 }
