01330799aec0b9cf2fe882320c702403852bbf05
Simon Knittel <hallo@simonknittel.de>
2021-03-12 20:04:34 +0100

    Add signed-cookie function

---
+++ functions/signed-cookie/signed-cookie.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto')
+
+const DOMAIN = process.env.DOMAIN
+const KEY_NAME = process.env.KEY_NAME
+const KEY_VALUE = process.env.KEY_VALUE
+
+let cookiePolicy = ''
+let signature = ''
+
+function base64(string) {
+  return Buffer
+    .from(string)
+    .toString('base64')
+}
+
+function urlSafe(base64) {
+  return base64
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+}
+
+function createCookiePolicy() {
+  const urlPrefix = 'https://' + DOMAIN
+
+  const expiresDate = new Date()
+  expiresDate.setDate(expiresDate.getDate() + 1)
+
+  const expires = Math.floor(expiresDate.getTime() / 1000)
+
+  cookiePolicy = `URLPrefix=${ urlSafe(base64(urlPrefix)) }:Expires=${ expires }:KeyName=${ KEY_NAME }`
+}
+
+function createSignature() {
+  signature = crypto
+    .createHmac('sha1', Buffer.from(KEY_VALUE, 'base64'))
+    .update(cookiePolicy)
+    .digest('base64')
+}
+
+exports.main = (_, res) => {
+  createCookiePolicy()
+  createSignature()
+
+  res
+    .cookie('Cloud-CDN-Cookie', `${ cookiePolicy }:Signature=${ urlSafe(signature) }`, {
+      domain: '.' + DOMAIN,
+      path: '/',
+      secure: true,
+      encode: String,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24,
+    })
+    .redirect(303, 'https://' + DOMAIN)
+}
