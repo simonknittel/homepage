ca9642fe74711eaedee87a36e273ad6b293ba521
Simon Knittel <hallo@simonknittel.de>
2021-03-13 12:31:39 +0100

    Add cors to form-submit

--- functions/form-submit/index.js
+++ functions/form-submit/index.js
@@ -1,6 +1,20 @@
 const Airtable = require('airtable')
 
 exports.main = function(req, res) {
+  res.removeHeader('X-Powered-By')
+
+  const allowedOrigins = process.env.CORS_ORIGINS.split(',')
+  allowedOrigins.includes(req.get('Origin'))
+
+  res.append('Access-Control-Allow-Origin', req.get('Origin'))
+  res.append('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204)
+    return
+  }
+
   const base = new Airtable().base(req.body.baseId)
 
   const fields = {}
