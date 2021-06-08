36d95b0a621d5db3bab5c8ed4a1e7c4cd366dea9
Simon Knittel <hallo@simonknittel.de>
2021-06-08 22:23:45 +0200

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ const plugins = [
   {
     resolve: `gatsby-plugin-manifest`,
     options: {
-      name: `Simon Knittel - Web Developer`,
+      name: `Simon Knittel - Webentwickler`,
       short_name: `Simon Knittel`,
       start_url: `/`,
       background_color: `#fff5cc`,
