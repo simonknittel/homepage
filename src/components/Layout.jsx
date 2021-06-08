8715d5714bcba499c21abdf0467fec4c6679c73a
Simon Knittel <hallo@simonknittel.de>
2021-06-08 08:32:32 +0200

    Fix height issue for mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,19 @@
-// import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import * as React from "react"
-// import { Footer } from "./Footer"
+import { Footer } from "./Footer"
 import { Header } from "./Header"
 
 export function Layout({
   children,
   hideHeader = false
 }) {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     datoCmsGlobalConfiguration {
-  //       footer
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query {
+      datoCmsGlobalConfiguration {
+        footer
+      }
+    }
+  `)
 
   return <>
     { hideHeader === true ? null : <Header /> }
