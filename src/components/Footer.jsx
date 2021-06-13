dc4b9b2d6b5c012072bc99433b00479fdf01568d
Simon Knittel <hallo@simonknittel.de>
2021-06-13 13:13:19 +0200

    Add IPFS tooltip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,46 @@ export function Footer() {
       <p className="Footer__font-awesome">Die Icons werden bereitgestellt durch Font Awesome (<a href="https://fontawesome.com/license/free" rel="noopener nofollow">Lizenz</a>).</p>
     </div>
 
-    { process.env.GATSBY_IPFS_CID ?
+    {/* { process.env.GATSBY_IPFS_CID ? */}
+    { true ?
       <div className="Footer__right">
         <div className="Footer__ipfs">
           <p className="Footer__ipfs__intro">Diese Seite ist über IPFS erreichbar:</p>
-          <button className="Footer__ipfs__button">
+          <div className="Footer__ipfs__button">
             <IpfsIcon />
-          </button>
+
+            <div className="Footer__ipfs__tooltip">
+              <ul>
+                <li className="Footer__ipfs__cid">
+                  <span>CID</span>
+                  <code>{ process.env.GATSBY_IPFS_CID }</code>
+                </li>
+
+                {/* <li className="Footer__ipfs__ud">
+                  <span>Unstoppable Domains</span>
+                  <a href="https://simonknittel.crypto">simonknittel.crypto</a>
+                </li> */}
+
+                <li className="Footer__ipfs__protocol">
+                  <a href={ `ipfs://${ process.env.GATSBY_IPFS_CID }` }>ipfs:// protocol</a>
+                </li>
+
+                <li className="Footer__ipfs__gateways">
+                  <span>Gateways</span>
+
+                  <ul>
+                    <li>
+                      <a href={ `https://ipfs.io/ipfs/${ process.env.GATSBY_IPFS_CID }` }>ipfs.io</a>
+                    </li>
+
+                    <li>
+                      <a href={ `https://cloudflare-ipfs.com/ipfs/${ process.env.GATSBY_IPFS_CID }` }>Cloudflare</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     : null }
