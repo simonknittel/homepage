a82cc3706f2536d93b62ebed77b42f2bd6af8ab7
Simon Knittel <hallo@simonknittel.de>
2021-06-13 13:20:15 +0200

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,7 @@ export function Footer() {
       <p className="Footer__font-awesome">Die Icons werden bereitgestellt durch Font Awesome (<a href="https://fontawesome.com/license/free" rel="noopener nofollow">Lizenz</a>).</p>
     </div>
 
-    {/* { process.env.GATSBY_IPFS_CID ? */}
-    { true ?
+    { process.env.GATSBY_IPFS_CID ?
       <div className="Footer__right">
         <div className="Footer__ipfs">
           <p className="Footer__ipfs__intro">Diese Seite ist über IPFS erreichbar:</p>
