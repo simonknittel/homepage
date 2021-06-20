de2b6abc089e40258d76ddc020dbd35aaecbfe34
Simon Knittel <hallo@simonknittel.de>
2021-06-20 09:47:09 +0200

    Fix IPFS tooltip for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,10 @@ import * as React from "react"
 import IpfsIcon from '../images/ipfs.inline.svg'
 
 export function Footer() {
+  function onClick(e) {
+    e.currentTarget.classList.toggle('Footer__ipfs__button--active')
+  }
+
   return <footer className="Footer">
     <div className="Footer__left">
       <p className="Footer__font-awesome">Die Icons werden bereitgestellt durch Font Awesome (<a href="https://fontawesome.com/license/free" rel="noopener nofollow">Lizenz</a>).</p>
@@ -12,7 +16,8 @@ export function Footer() {
       <div className="Footer__right">
         <div className="Footer__ipfs">
           <p className="Footer__ipfs__intro">Diese Seite ist über IPFS erreichbar:</p>
-          <div className="Footer__ipfs__button">
+
+          <div className="Footer__ipfs__button" onClick={ onClick }>
             <IpfsIcon />
 
             <div className="Footer__ipfs__tooltip">
